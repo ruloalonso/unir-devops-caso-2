f08bffe68144ae7ba40bc7ce73789be8dccf1c48
Raul Alonso <c754s@ferrovial.com>
2022-03-07 23:16:52 +0100

    vars updated

--- terraform/correccion-vars.tf
+++ terraform/correccion-vars.tf
@@ -1,23 +1,23 @@
 variable "location" {
   type = string
   description = "Región de Azure donde crearemos la infraestructura"
-  default = "<YOUR REGION>"
+  default = "westeurope"
 }
 
 variable "storage_account" {
   type = string
   description = "Nombre para la storage account"
-  default = "<STORAGE ACCOUNT NAME>"
+  default = "unir-raul-alonso-sa"
 }
 
 variable "public_key_path" {
   type = string
   description = "Ruta para la clave pública de acceso a las instancias"
-  default = "~/.ssh/id_rsa.pub" # o la ruta correspondiente
+  default = "~/.ssh/id_rsa.pub"
 }
 
 variable "ssh_user" {
   type = string
   description = "Usuario para hacer ssh"
-  default = "<SSH USER>"
+  default = "rulo"
 }
