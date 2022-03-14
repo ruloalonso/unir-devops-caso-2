db8d483d71c3f8f03af9bbe4ac0d2b95c11356cb
Raul Alonso <c754s@ferrovial.com>
2022-03-14 23:15:43 +0100

    comment removed

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# añadir tantas líneas como sean necesarias para el correcto despligue
 ansible-playbook -i hosts addRepos.yaml
 ansible-playbook -i hosts install.yaml
 ansible-playbook -i hosts mongo-secret.yaml
