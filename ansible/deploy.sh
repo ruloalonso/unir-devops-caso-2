fbbc075407cc9870297eeb0df973a1516124fa0a
Raul Alonso <c754s@ferrovial.com>
2022-03-14 23:07:38 +0100

    ansible

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # añadir tantas líneas como sean necesarias para el correcto despligue
-ansible-playbook -i hosts -l XXXX playbook
-
+ansible-playbook -i hosts -l addRepos.yaml playbook
+ansible-playbook -i hosts -l install.yaml playbook
