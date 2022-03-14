d677dfb76100c67f514d7e6646471dfb922c7b34
Raul Alonso <c754s@ferrovial.com>
2022-03-14 23:12:30 +0100

    yamls moved

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 # añadir tantas líneas como sean necesarias para el correcto despligue
-ansible-playbook -i hosts -l addRepos.yaml playbook
-ansible-playbook -i hosts -l install.yaml playbook
+ansible-playbook -i hosts addRepos.yaml
+ansible-playbook -i hosts install.yaml
+ansible-playbook -i hosts mongo-secret.yaml
+ansible-playbook -i hosts mongo-config.yaml
+ansible-playbook -i hosts mongo.yaml
+ansible-playbook -i hosts webapp.yaml
